54f7f14e9df73ee1adbf3c5e37ce79c4241924a8
brokeninfp <brokeninfp@gmail.com>
2023-09-22 16:40:39 +0300

    chmod

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module for text_indentation method."""
+
+
 def text_indentation(text):
     """Method for adding 2 new lines after '.?:' chars.
 
@@ -17,6 +19,8 @@ def text_indentation(text):
         text = (delim + "\n\n").join(
             [line.strip(" ") for line in text.split(delim)])
     print(text, end="")
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/5-text_indentation.txt")
